e1d4863298edc352b45cc554fcd986c98b98d8f0
susan <susan.ra.evans@gmail.com>
2016-12-01 21:46:52 -0800

    cleaned up code

--- day2/part1.rb
+++ day2/part1.rb
@@ -1,7 +1,17 @@
 class Part1
 	def initialize(file_name)
-		@keypad = {1 => [-1, 1], 2 => [0, 1], 3 => [1, 1], 4 => [-1, 0], 5 => [0, 0], 6 => [1, 0], 7 => [-1, -1], 8 => [0, -1], 9 => [1, -1]}
-		@position = [0,0]
+		@keypad = {
+			1 => [-1, 1], 
+			2 => [0, 1], 
+			3 => [1, 1], 
+			4 => [-1, 0], 
+			5 => [0, 0], 
+			6 => [1, 0], 
+			7 => [-1, -1], 
+			8 => [0, -1], 
+			9 => [1, -1]
+		}
+		@position = [0, 0]
 		@code = ""
 		processFile(file_name)
 		output
@@ -12,14 +22,14 @@ class Part1
 			f.each_line do |line|
 				data = line.chomp.split("")
 				data.each do |instr|
-					update(instr)
+					process(instr)
 				end
 				@code += @keypad.key(@position).to_s
 			end
 		end
 	end
 
-	def update(instr)
+	def process(instr)
 		case instr
 		when "U"
 			@position[1] = [1, @position[1] + 1].min
--- day2/part2.rb
+++ day2/part2.rb
@@ -1,6 +1,20 @@
-class Part1
+class Part2
 	def initialize(file_name)
-		@keypad = {1 => [0, 2], 2 => [-1, 1], 3 => [0, 1], 4 => [1, 1], 5 => [-2, 0], 6 => [-1, 0], 7 => [0, 0], 8 => [1, 0], 9 => [2, 0], :A => [-1, -1], :B => [0, -1], :C => [1, -1], :D => [0, -2]}
+		@keypad = {
+			1 => [0, 2], 
+			2 => [-1, 1], 
+			3 => [0, 1], 
+			4 => [1, 1], 
+			5 => [-2, 0], 
+			6 => [-1, 0], 
+			7 => [0, 0], 
+			8 => [1, 0], 
+			9 => [2, 0], 
+			:A => [-1, -1], 
+			:B => [0, -1], 
+			:C => [1, -1], 
+			:D => [0, -2]
+		}
 		@position = [-2, 0]
 		@code = ""
 		processFile(file_name)
@@ -12,33 +26,32 @@ class Part1
 			f.each_line do |line|
 				data = line.chomp.split("")
 				data.each do |instr|
-					update(instr)
+					process(instr)
 				end
 				@code += @keypad.key(@position).to_s
 			end
 		end
 	end
 
-	def update(instr)
+	def process(instr)
 		case instr
 		when "U"
-			if @keypad.key([@position[0], @position[1] + 1]) != nil
+			if !@keypad.key([@position[0], @position[1] + 1]).nil?
 				@position[1] = @position[1] + 1
 			end
 		when "R"
-			if @keypad.key([@position[0] + 1, @position[1]]) != nil
+			if !@keypad.key([@position[0] + 1, @position[1]]).nil?
 				@position[0] = @position[0] + 1
 			end
 		when "D"
-			if @keypad.key([@position[0], @position[1] - 1]) != nil
+			if !@keypad.key([@position[0], @position[1] - 1]).nil?
 				@position[1] = @position[1] - 1
 			end
 		when "L"
-			if @keypad.key([@position[0] - 1, @position[1]]) != nil
+			if !@keypad.key([@position[0] - 1, @position[1]]).nil?
 				@position[0] = @position[0] - 1
 			end
 		end
-		# puts "x: #{@position[0]} y: #{@position[1]}"
 	end
 
 	def output
@@ -46,4 +59,4 @@ class Part1
 	end
 end
 
-Part1.new("input.txt")
+Part2.new("input.txt")
