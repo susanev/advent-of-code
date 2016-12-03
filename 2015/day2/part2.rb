0875ca43b6a14f6577c7db1995b3d32412f1cb68
susan <susan.ra.evans@gmail.com>
2016-12-03 00:35:33 -0800

    added 2015 day2 part 2 solution

--- 2015/day2/part2.rb
+++ 2015/day2/part2.rb
@@ -1,6 +1,6 @@
 class Part2
 	def initialize(file_name)
-		@paper = 0
+		@ribbon = 0
 		processFile(file_name)
 		output
 	end
@@ -8,15 +8,15 @@ class Part2
 	def processFile(file_name)
 		File.open(file_name, "r") do |f|
 			f.each_line do |line|
-				nums = line.split("x")
-				min = [nums[0] * [nums[1], nums[0] * nums[2], nums[1] * nums[2]]].min
-				@paper += 2 * nums[0] * nums[1] + 2 * nums[1] * nums[2] + 2 * nums[3] * nums[1] + min
+				dimensions = line.split("x").map(& :to_i)
+				perm = dimensions.map { |i| i * 2 }
+				@ribbon += perm.reduce(:+) - perm.max + dimensions.reduce(:*)
 			end
 		end
 	end
 
 	def output
-		puts "The elves should order #{@paper} square feet of wrapping paper"
+		puts "The elves should order #{@ribbon} feet of ribbon"
 	end
 end
 
