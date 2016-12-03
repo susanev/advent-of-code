fb1f1d5ec76118e13d7970806efa31695771da2c
susan <susan.ra.evans@gmail.com>
2016-12-02 21:36:57 -0800

    finished solution

--- day3/part1.rb
+++ day3/part1.rb
@@ -1,6 +1,6 @@
 class Part1
 	def initialize(file_name)
-
+		@count = 0
 		processFile(file_name)
 		output
 	end
@@ -8,19 +8,21 @@ class Part1
 	def processFile(file_name)
 		File.open(file_name, "r") do |f|
 			f.each_line do |line|
-				line.chomp.split("").each do |instr|
-					process(instr)
-				end
+				sides = line.chomp.split(" ").map(& :to_i)
+				validTriangle(sides[0], sides[1], sides[2])
 			end
 		end
 	end
 
-	def process(instr)
-
+	def validTriangle(side1, side2, side3)
+		if side1 + side2 > side3 && side1 + side3 > side2 && 
+				side2 + side3 > side1
+			@count+=1
+		end
 	end
 
 	def output
-
+		puts "#{@count} possible triangles"
 	end
 end
 
