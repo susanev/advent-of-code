b6131a9414f4c1fb7c74788ec147856f4dc34142
susan <susan.ra.evans@gmail.com>
2016-12-07 20:39:34 -0800

    file setup for 2016 day 8

---
+++ 2016/day8/part1.rb
@@ -0,0 +1,43 @@
+class Part1
+	def initialize(file_name)
+		@count = 0
+		processFile(file_name)
+		output
+	end
+
+	def processFile(file_name)
+		File.open(file_name, "r") do |f|
+			f.each_line do |line|
+				nets = line.scan(/(?<=\[)[a-z]+(?=\])/)
+				strs = line.scan(/[a-z]+(?=\[)/)
+				strs.push(line.scan(/[a-z]+$/)[0])
+				count(strs, nets)
+			end 
+		end
+	end
+
+	def count(strs, nets)
+		if strs.map { |str| abba(str)}.flatten.count(true) >= 1 && 
+				nets.map { |str| abba(str)}.flatten.count(true) == 0
+			@count += 1
+		end
+	end
+
+	def abba(str)
+		index = 0
+		(str.length - 3).times do
+			if (str[index] == str[index+3] && str[index+1] == str[index+2]) && 
+						str[index] != str[index+2]
+				return true
+			end
+			index += 1
+		end
+		return false
+	end
+
+	def output
+		puts "the original message is #{@count}"
+	end
+end
+
+Part1.new("input.txt")
---
+++ 2016/day8/readme.md
@@ -0,0 +1,12 @@
+# Advent of Code
+## Day 2
+
+### Part 1
+
+
+[Part 1 Solution](part1.rb)
+
+### Part 2
+
+
+[Part 2 Solution](part2.rb)
