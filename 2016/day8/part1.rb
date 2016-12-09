f8cec1840ef357994fcbe54ed178486207057596
susan <susan.ra.evans@gmail.com>
2016-12-08 20:04:19 -0800

    cleaned up code, and added comments

--- 2016/day8/part1.rb
+++ 2016/day8/part1.rb
@@ -1,12 +1,12 @@
-class Part1
-	CONST_SCREEN_WIDTH = 50
-	CONST_SCREEN_HEIGHT = 6
+# susan evans
+# last edited 12/08/2016
+# advent of code 2016, day 8, part 1
+# processes a file to count the number
+# of lights in a grid
 
+class Part1
 	def initialize(file_name)
-		@screen = []
-		CONST_SCREEN_HEIGHT.times do
-			@screen.push(Array.new(CONST_SCREEN_WIDTH, false))
-		end
+		@lights = 0
 		process_file(file_name)
 		output
 	end
@@ -14,51 +14,17 @@ class Part1
 	def process_file(file_name)
 		File.open(file_name, "r") do |f|
 			f.each_line do |line|
+				# scans file for rect and adds to @lights
 				if line.include? "rect"
-					update_lights(:rect, line[/(?<=rect )\d+/].to_i, line[/(?<=x)\d+/].to_i)
-				end
-				pos = line[/(?<=\=)\d+/].to_i
-				amt = line[/(?<=by )\d+/].to_i
-				if line.include? "row"
-					update_lights(:row, pos, amt)
-				else #col
-					update_lights(:col, pos, amt)
+					@lights += line[/(?<=rect )\d+/].to_i * line[/(?<=x)\d+/].to_i
 				end
 			end
 		end
 	end
 
-	def update_lights(type, pos, amt)
-		if type == :rect
-			(0...amt).each do |row|
-				(0...pos).each do |col|	 
-					@screen[row][col] = true
-				end
-			end
-		elsif type == :row
-			@screen[pos] = rotateArray(@screen[pos], amt)
-		elsif type == :col
-			arr = []
-			@screen.each do |row|
-				arr.push(row[pos])
-			end
-			new_arr = rotateArray(arr, amt)
-			@screen.each_with_index do |row, index|
-				row[pos] = new_arr[index]
-			end
-		end
-	end
-
-	def rotate_array(arr, n)
-		new_arr = Array.new(arr.length)
-		arr.length.times do |i|
-			new_arr[(n + i) % arr.length] = arr[i]
-		end
-		return new_arr
-	end
-
+	# displays the number of lit lights
 	def output
-		puts "#{@screen.flatten.map {|x| x ? 1 : 0}.reduce(:+)} lights are lit"
+		puts "#{@lights} lights are lit"
 	end
 end
 
--- 2016/day8/part2.rb
+++ 2016/day8/part2.rb
@@ -1,8 +1,18 @@
+# susan evans
+# last edited 12/08/2016
+# advent of code 2016, day 8, part 2
+# creates a grid of lights, then processes a file
+# that gives instructions about which lights should
+# be turned on, then displays the grid of lights
+
 class Part2
 	CONST_SCREEN_WIDTH = 50
 	CONST_SCREEN_HEIGHT = 6
 
 	def initialize(file_name)
+		# creates the @screen of CONST_SCREEN_HEIGHT x
+		# CONST_SCREEN_WIDTH, with all values set to false 
+		# (lights turned off)
 		@screen = []
 		CONST_SCREEN_HEIGHT.times do
 			@screen.push(Array.new(CONST_SCREEN_WIDTH, false))
@@ -11,52 +21,57 @@ class Part2
 		print_screen
 	end
 
+	# reads the file named file_name line by line
+	# using regex to pull out important information
 	def process_file(file_name)
 		File.open(file_name, "r") do |f|
 			f.each_line do |line|
 				if line.include? "rect"
-					update_lights(:rect, line[/(?<=rect )\d+/].to_i, line[/(?<=x)\d+/].to_i)
+					turn_on_rect(line[/(?<=rect )\d+/].to_i, line[/(?<=x)\d+/].to_i)
 				end
 				pos = line[/(?<=\=)\d+/].to_i
 				amt = line[/(?<=by )\d+/].to_i
 				if line.include? "row"
-					update_lights(:row, pos, amt)
+					rotate_lights(:row, pos, amt)
 				else #col
-					update_lights(:col, pos, amt)
+					rotate_lights(:col, pos, amt)
 				end
 			end
 		end
 	end
 
-	def update_lights(type, pos, amt)
-		if type == :rect
-			(0...amt).each do |row|
-				(0...pos).each do |col|	 
-					@screen[row][col] = true
-				end
+	# updates @screen with an area of
+	# width x height 
+	def turn_on_rect(width, height)
+		(0...width).each do |row|
+			(0...height).each do |col|	 
+				@screen[row][col] = true
 			end
-		elsif type == :row
-			@screen[pos] = rotateArray(@screen[pos], amt)
-		elsif type == :col
+		end
+	end
+
+	# rotates row or col of @screen
+	# using pos as the row or column
+	# and amt as the number of rotations
+	def rotate_lights(type, pos, amt)
+		if type == :row
+			@screen[pos] = @screen[pos].rotate!(-amt)
+		else # type == :col
+			# this can be done in place
+			# will revisit later
 			arr = []
 			@screen.each do |row|
 				arr.push(row[pos])
 			end
-			new_arr = rotateArray(arr, amt)
+			arr.rotate!(-amt)
 			@screen.each_with_index do |row, index|
-				row[pos] = new_arr[index]
+				row[pos] = arr[index]
 			end
 		end
 	end
 
-	def rotate_array(arr, n)
-		new_arr = Array.new(arr.length)
-		arr.length.times do |i|
-			new_arr[(n + i) % arr.length] = arr[i]
-		end
-		return new_arr
-	end
-
+	# prints a gird representing
+	# the screen of lights
 	def print_screen
 		@screen.each do |row|
 			row.each do |col|
