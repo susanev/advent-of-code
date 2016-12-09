f0a6a7f22e62c54ae34f7fcae2cc069cb6638ec1
susan <susan.ra.evans@gmail.com>
2016-12-09 10:32:13 -0800

    working solutions; need to be cleaned

--- 2016/day9/part1.rb
+++ 2016/day9/part1.rb
@@ -1,10 +1,11 @@
 # susan evans
 # last edited 12/08/2016
 # advent of code 2016, day 9, part 1
+# needs to be cleaned
 
 class Part1
 	def initialize(file_name)
-		@lights = 0
+		@length = 0
 		process_file(file_name)
 		output
 	end
@@ -12,13 +13,41 @@ class Part1
 	def process_file(file_name)
 		File.open(file_name, "r") do |f|
 			f.each_line do |line|
+				count(line)
+			end
+		end
+	end
 
+	# counts the length
+	def count(line)
+		index = 0
+		while (index < line.length)
+			if line[index] == "("
+				marker = line[index]
+				index += 1
+				while line[index] != ")"
+					marker << line[index]
+					index += 1
+				end
+				marker << line[index]
+				len = marker[/(?<=\()\d+/].to_i
+				repeat = marker[/\d+\)$/].to_i
+				index += 1
+				to_process = ""
+				len.times do 
+					to_process << line[index]
+					index += 1
+				end
+				@length += repeat * len
+			else
+				@length += 1
+				index += 1
 			end
 		end
 	end
 
 	def output
-		puts "#{@lights} lights are lit"
+		puts "length is #{@length}"
 	end
 end
 
---
+++ 2016/day9/part2.rb
@@ -0,0 +1,63 @@
+# susan evans
+# last edited 12/09/2016
+# advent of code 2016, day 9, part 2
+# needs to be cleaned
+
+class Part2
+	def initialize(file_name)
+		@length = 0
+		process_file(file_name)
+		output
+	end
+
+	def process_file(file_name)
+		File.open(file_name, "r") do |f|
+			f.each_line do |line|
+				@length += count(line)
+			end
+		end
+	end
+
+	# recursively counts the length
+	def count(line)
+		index = 0
+		new_len = 0
+		while (index < line.length)
+			if line[index] == "("
+				marker = line[index]
+				index += 1
+				while line[index] != ")"
+					marker << line[index]
+					index += 1
+				end
+				marker << line[index]
+				len = marker[/(?<=\()\d+/].to_i
+				repeat = marker[/\d+\)$/].to_i
+				index += 1
+				to_process = ""
+				len.times do 
+					to_process << line[index]
+					index += 1
+				end
+				# if no more markers, then add to length
+				# otherwise recursively call this method until
+				# no more markers
+				if !to_process.include? "("
+					new_len += repeat * len
+				else
+					new_len += repeat * build_input(to_process)
+				end
+			else
+				new_len += 1
+				index += 1
+			end
+		end
+		return new_len
+	end
+
+	def output
+		puts "length is #{@length}"
+	end
+end
+
+Part2.new("input.txt")
