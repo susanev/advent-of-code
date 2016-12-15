c5d4bc68df9253019e052f31013e176784cc11f1
susan <susan.ra.evans@gmail.com>
2016-12-14 18:40:57 -0800

    finds a path, but not the shortest path

--- 2016/day13/part1.rb
+++ 2016/day13/part1.rb
@@ -2,36 +2,27 @@
 # last edited 12/12/2016
 # advent of code 2016, day 123, part 1
 
+# finds a path, not the shortest path
+
 class Part1
 	CONST_HEIGHT = 45
 	CONST_WIDTH = 45
-	CONST_FAV_NUM = 1364
 
 	def initialize(file_name)
+		@fav_num = File.open(file_name, "r").first.to_i
 		@grid = Array.new(CONST_HEIGHT)
 		@grid.length.times do |i|
 			@grid[i] = Array.new(CONST_WIDTH)
 		end
 		build_grid
-		display_grid
-		puts find_path(1, 1, 0)
-		display_grid
-		#process_file(file_name)	
-		#output
-	end
-
-	def process_file(file_name)
-		File.open(file_name, "r") do |f|
-			f.each_line do |line|
-
-			end
-		end
+		@path = find_path(1, 1, 0)	
+		output
 	end
 
 	def build_grid
 		CONST_HEIGHT.times do |y|
 			CONST_WIDTH.times do |x|
-				bin = (x * x + 3 * x + 2 * x * y + y + y * y + CONST_FAV_NUM).to_s(2)
+				bin = (x * x + 3 * x + 2 * x * y + y + y * y + @fav_num).to_s(2)
 				if x == 31 && y == 39
 					@grid[y][x] = "?"
  				elsif bin.scan(/1/).count % 2 == 0
@@ -52,12 +43,9 @@ class Part1
 		end
 	end
 
-	def output
-		puts ""
-	end
-
 	def find_path (x, y, d)
-		if x < 0 || x > CONST_WIDTH - 1 || y < 0 || y > CONST_HEIGHT - 1
+		if x < 0 || x > CONST_WIDTH - 1 ||
+				y < 0 || y > CONST_HEIGHT - 1
 			return 0
 		elsif @grid[y][x] == "?"
 			return d
@@ -78,22 +66,10 @@ class Part1
 		end
 	end
 
-	# def find_path (x, y, d)
-	#   if x < 0 || x > CONST_WIDTH - 1 ||
-	# 			y < 0 || y > CONST_HEIGHT - 1 ||
-	# 			@grid[y][x] == "#" || @grid[y][x] == "O"
-	# 		return -1
- #  	elsif @grid[y][x] == "?"
- #  		return d
- #  	else
- #  		@grid[y][x] = "O"
-
-	# 		return [find_path(x + 1, y, d + 1),
-	# 				    find_path(x - 1, y, d + 1),
-	# 				    find_path(x, y + 1, d + 1),
-	# 				    find_path(x, y - 1, d + 1)].max
- #  	end
-	# end
+	def output
+		display_grid
+		puts "#{@path}"
+	end
 end
 
-Part1.new("input2.txt")
+Part1.new("input.txt")
