606bb5925df540ef0446f485ccd0d1ad38f94ebb
susan <susan.ra.evans@gmail.com>
2016-12-14 21:49:29 -0800

    added working solutions, still need comments

--- 2016/day15/part2.rb
+++ 2016/day15/part2.rb
@@ -1,3 +1,39 @@
 # susan evans
 # last edited 12/14/2016
 # advent of code 2016, day 15, part 2
+
+class Part2
+	def initialize(file_name)
+		@discs = {}
+		@n = 0
+		process_file(file_name)
+		find_n
+		output
+	end
+
+	def process_file(file_name)
+		File.open(file_name, "r") do |f|
+			f.each_line do |line|
+				info = line.scan(/\d+/).map(&:to_i)
+				@discs[info[0]] = {:positions => info[1], :start => info[3]}
+			end
+		end
+	end
+
+	def find_n
+		found = nil
+		until found
+			@n += 1
+			found = true
+			(1..@discs.length).each do |i|
+				found = found && (@n + i + @discs[i][:start]) % @discs[i][:positions] == 0 
+			end		
+		end
+	end
+
+	def output
+		puts "press the button at time=#{@n}"
+	end
+end
+
+Part2.new("input2.txt")
