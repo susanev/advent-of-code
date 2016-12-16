e8a045d560bc93c3705fc1f02df93caa847b34ff
susan <susan.ra.evans@gmail.com>
2016-12-15 23:02:28 -0800

    working solutions; still need comments

--- 2016/day16/part2.rb
+++ 2016/day16/part2.rb
@@ -1,3 +1,60 @@
 # susan evans
 # last edited 12/15/2016
 # advent of code 2016, day 16, part 2
+
+class Part2
+	CONST_DISK_SIZE = 35651584
+
+	def initialize(file_name)
+		@data = File.open(file_name, "r").first
+		generate_data
+		find_valid_checksum
+		output
+	end
+
+	def generate_data 
+		until @data.length >= CONST_DISK_SIZE
+			next_data = reverse_inverse(@data)
+			@data << "0"
+			@data << next_data
+		end
+		@data = @data[0, CONST_DISK_SIZE]
+	end
+
+	def reverse_inverse(str)
+		new_str = ""
+		pos = str.length - 1
+		while pos > -1
+			str[pos] == "0" ? new_str << "1" : new_str << "0"
+			pos -= 1
+		end
+		return new_str
+	end
+
+	def find_valid_checksum
+		@checksum = find_checksum(@data)
+		until @checksum.length % 2 == 1
+			@checksum = find_checksum(@checksum)
+		end
+	end
+
+	def find_checksum(data)
+		pos = 0
+		checksum = ""
+		while pos < data.length - 1
+			if data[pos] == data[pos + 1]
+				checksum << "1"
+			else
+				checksum << "0"
+			end
+			pos += 2
+		end
+		@checksum = checksum
+	end
+
+	def output
+		puts "The correct check sum is: #{@checksum}"
+	end
+end
+
+Part2.new("input.txt")
