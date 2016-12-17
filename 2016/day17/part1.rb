47074246ccf529f538ace28a0c7b97ef68d61020
susan <susan.ra.evans@gmail.com>
2016-12-16 20:16:43 -0800

    file setup for 2016 day17

---
+++ 2016/day17/part1.rb
@@ -0,0 +1,24 @@
+# susan evans
+# last edited 12/17/2016
+# advent of code 2016, day 17, part 1
+
+class Part1
+	def initialize(file_name)
+		process_file
+		output
+	end
+
+	def process_file(file_name)
+		File.open(file_name, "r") do |f|
+			f.each_line do |line|
+
+			end
+		end
+	end
+
+	def output
+		puts ""
+	end
+end
+
+Part1.new("input.txt")
---
+++ 2016/day17/part2.rb
@@ -0,0 +1,3 @@
+# susan evans
+# last edited 12/16/2016
+# advent of code 2016, day 17, part 2
