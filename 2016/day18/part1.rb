ee4f23ec907220d569be5b7cde900e6de62a2027
susan <susan.ra.evans@gmail.com>
2016-12-17 22:06:48 -0800

    working solutions, part2 is sloooow

--- 2016/day18/part1.rb
+++ 2016/day18/part1.rb
@@ -3,21 +3,45 @@
 # advent of code 2016, day 18, part 1
 
 class Part1
+	CONST_ROWS = 40
+	CONST_COLS = 100
+	CONST_TRAPS = ["^^.", ".^^", "^..", "..^"]
+
 	def initialize(file_name)
-		process_file
+		@safe_tiles = count_safe_tiles(file_name)
 		output
 	end
 
-	def process_file(file_name)
-		File.open(file_name, "r") do |f|
-			f.each_line do |line|
+	def count_safe_tiles(file_name)
+		prev_row = ".#{File.open(file_name, "r").first}."
+		count = prev_row.count(".") - 2
+		(CONST_ROWS - 1).times do
+			new_row = ""
+			(1..CONST_COLS).each do |col|
+				if is_trap(prev_row, col)
+					new_row << "^"	
+				else
+					new_row << "."
+				end
+			end
+			count += new_row.count(".")
+			prev_row = ".#{new_row}."
+		end
+		return count
+	end
 
+	# pre-condition, col > 0 && col < row.length - 1
+	def is_trap(row, col)
+		CONST_TRAPS.each do |t|
+			if row[col - 1, 3] == t
+				return true
 			end
 		end
+		return false
 	end
 
 	def output
-		puts ""
+		puts "There are #{@safe_tiles} safe tiles"
 	end
 end
 
