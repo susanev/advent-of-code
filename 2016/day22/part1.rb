4821d77afae6241acc4dd24ad89c9ab171b56b5a
susan <susan.ra.evans@gmail.com>
2016-12-23 13:33:57 -0800

    working solution

--- 2016/day22/part1.rb
+++ 2016/day22/part1.rb
@@ -1,9 +1,11 @@
 # susan evans
-# last edited 12/21/2016
+# last edited 12/23/2016
 # advent of code 2016, day 22, part 1
 
 class Part1
 	def initialize(file_name)
+		@viable_pairs = []
+		@data = []
 		process_file(file_name)
 		output
 	end
@@ -11,13 +13,32 @@ class Part1
 	def process_file(file_name)
 		File.open(file_name, "r") do |f|
 			f.each_line do |line|
+				if line.include? "/dev/grid/node"
+					add_data(line)
+				end
+			end
+		end
+	end
 
+	def add_data(line)
+		line = line.split(" ").map(&:to_i)
+		new_data = {used: line[2], avail: line[3]}
+		count_viable_pairs(new_data)
+		@data.push(new_data)
+	end
+
+	def count_viable_pairs(d2)
+		@data.each_with_index do |d1, i|
+			if d1[:used] != 0 && d1[:used] <= d2[:avail]			
+				@viable_pairs.push([d1, d2])
+			elsif d2[:used] != 0 && d2[:used] <= d1[:avail]
+				@viable_pairs.push([d2, d1])
 			end
 		end
 	end
 
 	def output
-		puts ""
+		puts "There are #{@viable_pairs.length} viable pairs"
 	end
 end
 
