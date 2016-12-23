eba644a7a4e202cb9f1d4df09402d662cb224004
susan <susan.ra.evans@gmail.com>
2016-12-23 11:30:27 -0800

    cleaned, and refactored

--- 2016/day23/part1.rb
+++ 2016/day23/part1.rb
@@ -1,14 +1,15 @@
 # susan evans
-# last edited 12/22/2016
+# last edited 12/23/2016
 # advent of code 2016, day 23, part 1
 
-# works, super sloppy .. will come back
-
 class Part1
+	CONST_TOGGLE_MAP = {inc: :dec, dec: :inc, tgl: :inc, jnz: :cpy, cpy: :jnz}
+	CONST_MAX_INPUTS = 3
+	CONST_A_START_VALUE = 7
+
 	def initialize(file_name)
 		@instructions = []
-		@registers = {a: 7, b: 0, c: 0, d: 0}
-		@toggle_map = {inc: :dec, dec: :inc, tgl: :inc, jnz: :cpy, cpy: :jnz}
+		@registers = {a: CONST_A_START_VALUE, b: 0, c: 0, d: 0}
 		process_file(file_name)	
 		process_instructions
 		output
@@ -25,9 +26,9 @@ class Part1
 	def build_instruction(line)
 		instr = {}
 		instr[:type] = line[0].to_sym
-		instr[1] = convert(line[1])
-		instr[2] = convert(line[2]) if line[2]
-		instr[3] = convert(line[3]) if line[3]
+		(1..CONST_MAX_INPUTS).each do |i|
+			instr[i] = convert(line[i]) if line[i]
+		end
 		@instructions.push(instr)
 	end
 
@@ -35,36 +36,24 @@ class Part1
 		pos = 0
 		while pos < @instructions.length
 			instr = @instructions[pos]
-			if instr[:type] == :cpy
-				if !is_number?(instr[2])
-					@registers[instr[2]] = is_number?(instr[1])? instr[1] : @registers[instr[1]]
-				end
+			if instr[:type] == :cpy && !is_number?(instr[2])
+				@registers[instr[2]] = is_number?(instr[1])? instr[1] : @registers[instr[1]]
 			elsif instr[:type] == :inc
 				@registers[instr[1]] += 1
 			elsif instr[:type] == :dec
 				@registers[instr[1]] -= 1
-			elsif instr[:type] == :tgl
-				toggle = pos + @registers[instr[1]]
-				if toggle < @instructions.length
-					@instructions[toggle][:type] = @toggle_map[@instructions[toggle][:type]]
-				end
-			elsif @registers[instr[1]] != 0 #jnz
-				if is_number?(instr[2]) && (pos + instr[2] - 1) < @instructions.length 
-						pos += instr[2] - 1
-				elsif (pos + @registers[instr[2]] - 1) < @instructions.length 
-					pos += @registers[instr[2]] - 1
-				end
+			elsif instr[:type] == :tgl && (tgl = pos + @registers[instr[1]]) < @instructions.length
+				@instructions[tgl][:type] = CONST_TOGGLE_MAP[@instructions[tgl][:type]]
+			elsif instr[:type] == :jnz && @registers[instr[1]] != 0 
+				inc = is_number?(instr[2]) ? instr[2] : @registers[instr[2]]
+				pos += inc - 1 if (pos + inc - 1) < @instructions.length
 			end
 			pos += 1
 		end
 	end
 
 	def convert(value)
-		if is_number? value
-			return value.to_i
-		else
-			return value.to_sym
-		end
+		is_number?(value)? value.to_i : value.to_sym
 	end
 
 	def is_number?(value)
--- 2016/day23/part2.rb
+++ 2016/day23/part2.rb
@@ -1,16 +1,18 @@
 # susan evans
-# last edited 12/22/2016
+# last edited 12/23/2016
 # advent of code 2016, day 23, part 2
 
-# works, super sloppy .. will come back
-# edited the file, to include mul
-
 class Part2
+	CONST_TOGGLE_MAP = {inc: :dec, dec: :inc, tgl: :inc, jnz: :cpy, cpy: :jnz}
+	CONST_MULT = [:cpy, :inc, :dec, :jnz, :dec, :jnz]
+	CONST_MAX_INPUTS = 3
+	CONST_A_START_VALUE = 12
+
 	def initialize(file_name)
 		@instructions = []
-		@registers = {a: 12, b: 0, c: 0, d: 0}
-		@toggle_map = {inc: :dec, dec: :inc, tgl: :inc, jnz: :cpy, cpy: :jnz}
+		@registers = {a: CONST_A_START_VALUE, b: 0, c: 0, d: 0}
 		process_file(file_name)	
+		add_mul
 		process_instructions
 		output
 	end
@@ -26,10 +28,32 @@ class Part2
 	def build_instruction(line)
 		instr = {}
 		instr[:type] = line[0].to_sym
-		instr[1] = convert(line[1])
-		instr[2] = convert(line[2]) if line[2]
-		instr[3] = convert(line[3]) if line[3]
+		(1..CONST_MAX_INPUTS).each do |i|
+			instr[i] = convert(line[i]) if line[i]
+		end
 		@instructions.push(instr)
+	end
+
+	# if a sequence of instructions representing multiplication
+	# are found, it is replaced with a new mul instruction
+	def add_mul
+		pos = 0
+		while pos < @instructions.length - 6
+			six = @instructions[pos, 6]
+			if six.map { |instr| instr[:type] } == CONST_MULT &&
+					[six[0][1], six[0][2], six[1][1], six[4][1]].uniq.length == 4 &&
+					[six[0][2], six[2][1], six[2][1]].uniq.length == 1 &&
+					six[4][1] == six[5][1] &&
+					six[3][2] == -2 && six[5][2] == -5
+				@instructions[pos] = {:type => :mul, 1 => six[0][1], 2 => six[4][1], 3 => six[1][1]}
+				@instructions[pos + 1] = {:type => :cpy, 1 => 0, 2 => six[2][1]}
+				@instructions[pos + 2] = {:type => :cpy, 1 => 0, 2 => six[4][1]}
+				((pos + 3)..(pos + 5)).each do |i|
+					@instructions[i] = {:type => :none}
+				end
+			end
+			pos += 1
+		end
 	end
 
 	def process_instructions
@@ -37,37 +61,25 @@ class Part2
 		while pos < @instructions.length
 			instr = @instructions[pos]
 			if instr[:type] == :mul
-				@registers[instr[1]] += (@registers[instr[2]] * @registers[instr[3]])
-			elsif instr[:type] == :cpy
-				if !is_number?(instr[2])
-					@registers[instr[2]] = is_number?(instr[1])? instr[1] : @registers[instr[1]]
-				end
+				@registers[instr[3]] += @registers[instr[1]] * @registers[instr[2]]
+			elsif instr[:type] == :cpy && !is_number?(instr[2])
+				@registers[instr[2]] = is_number?(instr[1])? instr[1] : @registers[instr[1]]
 			elsif instr[:type] == :inc
 				@registers[instr[1]] += 1
 			elsif instr[:type] == :dec
 				@registers[instr[1]] -= 1
-			elsif instr[:type] == :tgl
-				toggle = pos + @registers[instr[1]]
-				if toggle < @instructions.length
-					@instructions[toggle][:type] = @toggle_map[@instructions[toggle][:type]]
-				end
-			elsif @registers[instr[1]] != 0 #jnz
-				if is_number?(instr[2]) && (pos + instr[2] - 1) < @instructions.length 
-						pos += instr[2] - 1
-				else (pos + @registers[instr[2]] - 1) < @instructions.length 
-					pos += @registers[instr[2]] - 1
-				end
+			elsif instr[:type] == :tgl && (tgl = pos + @registers[instr[1]]) < @instructions.length
+				@instructions[tgl][:type] = CONST_TOGGLE_MAP[@instructions[tgl][:type]]
+			elsif instr[:type] == :jnz && @registers[instr[1]] != 0 
+				inc = is_number?(instr[2]) ? instr[2] : @registers[instr[2]]
+				pos += inc - 1 if (pos + inc - 1) < @instructions.length
 			end
 			pos += 1
 		end
 	end
 
 	def convert(value)
-		if is_number? value
-			return value.to_i
-		else
-			return value.to_sym
-		end
+		is_number?(value)? value.to_i : value.to_sym
 	end
 
 	def is_number?(value)
@@ -79,4 +91,4 @@ class Part2
 	end
 end
 
-Part2.new("input2.txt")
+Part2.new("input.txt")
