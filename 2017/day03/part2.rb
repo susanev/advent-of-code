4f88e455a5106bb71c27a80fe711eb6d29214f2f
susanev <susan.ra.evans@gmail.com>
2017-12-03 10:38:21 -0800

    some refactoring

--- 2017/day03/part2.rb
+++ 2017/day03/part2.rb
@@ -1,75 +1,50 @@
-# super sloppy, needs to be refactored
-
 class Part2
 	def initialize(file_name)
+		@order = [:right, :up, :left, :down]
 		@current = :right
+		@dirs = { right: {max: 1, current: 0}, left: {max: 2, current: 0}, up: {max: 1, current: 0}, down: {max: 2, current: 0}}
+		@width = nil
 		@grid = nil
-		@r = 1
-		@l = 2
-		@u = 1
-		@d = 2
-		@sum = 0
 		processFile(file_name)
-		output
 	end
 
 	def processFile(file_name)
 		File.open(file_name, "r") do |f|
 			f.each_line do |line|
-				process(line.to_i)
+				num = line.to_i
+				@width = Math.sqrt(num).ceil
+				@grid = Array.new(@width) {Array.new(@width, 0)}
+				process(num)
 			end
 		end
 	end
 
 	def process(num)
-		r = 0
-		l = 0
-		u = 0
-		d = 0
-		@width = Math.sqrt(num).ceil
-		@grid = Array.new(@width) {Array.new(@width, 0)}
 		x_pos = @width / 2 
 		y_pos = @width / 2
 		@grid[y_pos][x_pos] = 1
-		while @sum < num
-			@grid[y_pos][x_pos] = sum(x_pos, y_pos)
+		sum = 0
+		while sum < num
+			sum = calc_sum(x_pos, y_pos)
+			@grid[y_pos][x_pos] = sum
 			if @current == :right
 				x_pos += 1
-				r += 1
-				if r == @r
-					r = 0
-					@r += 2
-					@current = :up
-				end
+				update_dirs(:right)
 			elsif @current == :up
 				y_pos -= 1
-				u += 1
-				if u == @u
-					u = 0
-					@u += 2
-					@current = :left
-				end
+				update_dirs(:up)
 			elsif @current == :left
 				x_pos -= 1
-				l += 1
-				if l == @l
-					l = 0
-					@l += 2
-					@current = :down
-				end
+				update_dirs(:left)
 			else 
 				y_pos += 1
-				d += 1
-				if d == @d
-					d = 0
-					@d += 2
-					@current = :right
-				end
+				update_dirs(:down)
 			end			
 		end
+		puts "#{sum}"
 	end
 
-	def sum(x_pos, y_pos)
+	def calc_sum(x_pos, y_pos)
 		sum = @grid[y_pos][x_pos]
 		if x_pos + 1 < @width
 			sum += @grid[y_pos][x_pos + 1]
@@ -95,12 +70,16 @@ class Part2
 		if y_pos + 1 < @width && x_pos - 1 > -1 
 			sum += @grid[y_pos + 1][x_pos - 1]
 		end
-		@sum = sum
 		return sum
 	end
 
-	def output
-		puts "#{@sum}"
+	def update_dirs(dir)
+		@dirs[dir][:current] += 1
+		if @dirs[dir][:current] == @dirs[dir][:max]
+			@dirs[dir][:current] = 0
+			@dirs[dir][:max] += 2
+			@current = @order[(@order.index(dir) + 1) % @order.length]
+		end
 	end
 end
 
