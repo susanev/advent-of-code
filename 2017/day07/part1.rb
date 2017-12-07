70f9528c658fe465b656fc70841262e514aecaf7
susanev <susan.ra.evans@gmail.com>
2017-12-06 22:01:49 -0800

    part1 soln

---
+++ 2017/day07/part1.rb
@@ -0,0 +1,39 @@
+# susan evans
+# last edited 12/06/2017
+# advent of code 2017, day 7, part 1
+require 'tree'  
+
+class Part1
+	def initialize(file_name)
+		@keys = []
+		@vals = []
+		processFile(file_name)
+		output
+	end
+
+	def processFile(file_name)
+		File.open(file_name, "r") do |f|
+			f.each_line do |line|
+				line.chomp!
+				if !line.index("->").nil?
+					@keys.push(line[/[a-z]+/])
+					line[line[/[a-z]+[\s(\d*)]/].length + 
+								line[/\d+/].length + 
+								6...line.length].split(", ").each do |item|
+						@vals.push(item)
+					end
+				end
+			end
+		end
+	end
+
+	def output
+		@keys.each do |key|
+			if @vals.index(key).nil?
+				puts key
+			end
+		end
+	end
+end
+
+Part1.new("input.txt")
