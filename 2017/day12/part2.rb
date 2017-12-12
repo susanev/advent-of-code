58eaa94398715c9b8e11a51e3930a01f839cba1e
susanev <susan.ra.evans@gmail.com>
2017-12-11 22:24:31 -0800

    2017 day12 puzzles and solns

---
+++ 2017/day12/part2.rb
@@ -0,0 +1,68 @@
+# susan evans
+# last edited 12/11/2017
+# advent of code 2017, day 12, part 2
+
+class Part2
+	def initialize(file_name)
+		@all_groups = []
+		@programs = {}
+		@count = 0
+		processFile(file_name)
+		count_programs
+		output
+	end
+
+	def processFile(file_name)
+		File.open(file_name, "r") do |f|
+			f.each_line do |line|
+				line.chomp!
+				prog = line[/\d+\s/].to_i
+				arrow_index = line.index("<->")
+				to = line[arrow_index + 4..line.length].split(",").map(&:to_i)
+				@programs[prog] = to
+			end
+		end
+	end
+
+	def count_programs
+		n = 0
+		while n < 2000
+			if !@all_groups.include?(n)
+				@connections = [n]
+				process(n)
+				@all_groups.push(*@connections)
+				@count += 1
+			end
+			n += 1
+		end
+	end
+
+	def process(n)
+		prev = nil
+		while prev != @connections.sort.to_s
+			prev = @connections.clone.sort.to_s
+			@programs.clone.each do |prog, to|
+				if prog == n
+					to.each do |val|
+						@connections.push(val)
+					end
+				elsif to.include?(n)
+					@connections.push(prog)
+				else
+					to.each do |val|
+						if @connections.include?(val)
+							@connections.push(prog)
+						end
+					end
+				end
+			end
+			@connections.uniq!
+		end
+	end
+
+	def output
+		puts "#{@count}"
+	end
+end
+
+Part2.new("input.txt")
