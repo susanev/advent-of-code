97fd85fbaae722d7e2b787058d45fca2cfe7a55b
susanev <susan.ra.evans@gmail.com>
2017-12-18 22:04:32 -0800

    links solns puzzles for 2017 day 19, ps super hacky

---
+++ 2017/day19/part1.rb
@@ -0,0 +1,86 @@
+# susan evans
+# last edited 12/18/2017
+# advent of code 2017, day 19, part 1
+
+class Part1
+	def initialize(file_name)
+		@grid = []
+		@path = []
+		@x_pos = 0
+		@y_pos = 0
+		@steps = 0
+		@dir = :south
+		processFile(file_name)
+		find_start
+		find_path
+		output
+	end
+
+	def processFile(file_name)
+		line_index = 0
+		File.open(file_name, "r") do |f|
+			f.each_line do |line|
+				@grid.push(line.split(""))
+			end
+		end
+	end
+
+	def find_start
+		current = nil
+		while current != "|"
+			@x_pos += 1
+			current = @grid[0][@x_pos]
+		end
+	end
+
+	def find_path
+		while !@path.join.include?("I")
+			if @y_pos + 1 >= @grid.length || @grid[@y_pos + 1][@x_pos] == " "
+				up_down(-1)
+				@dir = :south
+			else
+				up_down(1)
+				@dir = :north
+			end
+			if @x_pos + 1 >= @grid[@y_pos].length || @grid[@y_pos][@x_pos + 1] == " "
+				left_right(-1)
+				@dir = :west
+			else
+				left_right(1)
+				@dir = :east
+			end
+		end
+	end
+
+	def up_down(dir)
+		while @y_pos + dir < @grid.length && 
+				("|+-".include?(@grid[@y_pos + dir][@x_pos]) || is_letter(@grid[@y_pos + dir][@x_pos])) 
+			if is_letter(@grid[@y_pos + dir][@x_pos])
+				@path.push(@grid[@y_pos + dir][@x_pos])
+				@grid[@y_pos + dir][@x_pos] = " "
+			end
+			@y_pos += dir
+		end
+	end
+
+	def left_right(dir)
+		while @x_pos + dir < @grid[@y_pos].length && 
+				("|+-".include?(@grid[@y_pos][@x_pos + dir]) || is_letter(@grid[@y_pos][@x_pos + dir]))
+			if is_letter(@grid[@y_pos][@x_pos + dir])
+				@path.push(@grid[@y_pos][@x_pos + dir])
+				@grid[@y_pos][@x_pos + dir] = " "
+			end
+			@x_pos += dir
+		end
+	end
+
+	def is_letter(item)
+		return !"|+- ".include?(item)
+	end
+
+	def output
+		puts "#{@path.join}"
+	end
+end
+
+Part1.new("input.txt")
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ _This repository holds my solutions (written in Ruby) for [Advent of Code](http:
 | [Day 1](2017/day01) | [Day 6](2017/day06) | [Day 11](2017/day11) | [Day 16](2017/day16) | |
 | [Day 2](2017/day02) | [Day 7](2017/day07) | [Day 12](2017/day12) | [Day 17](2017/day17) | |
 | [Day 3](2017/day03) | [Day 8](2017/day08) | [Day 13](2017/day13) | [Day 18](2017/day18) | |
-| [Day 4](2017/day04) | [Day 9](2017/day09) | [Day 14](2017/day14) | | |
+| [Day 4](2017/day04) | [Day 9](2017/day09) | [Day 14](2017/day14) | [Day 19](2017/day19) | |
 | [Day 5](2017/day05) | [Day 10](2017/day10) | [Day 15](2017/day15) | | |
 
 
