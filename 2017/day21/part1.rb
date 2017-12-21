d6afd99ecdbaed5e41f560282f52c20a29b27b87
susan <susan.ra.evans@gmail.com>
2017-12-21 14:40:21 -0800

    still very broken

--- 2017/day21/part1.rb
+++ 2017/day21/part1.rb
@@ -7,34 +7,33 @@ class Part1
 		@matrix = [".", "#", ".", ".", ".", "#", "#", "#", "#"]
 		@rules = {}
 		processFile(file_name)
-		stuff = []
-		four_split(@rules[@matrix]).each do |item|
-			stuff += four_split(@rules[@rules[item]])
+		arr = four_split(@rules[@matrix])
+		arr.each_with_index do |item, item_index|
+			arr[item_index] = @rules[item]
 		end
+		puts arr.flatten.count("#")
 
-		count = 0
-		stuff.each do |section|
-			count += @rules[@rules[section]].count("#")
+		arr.each_with_index do |item, item_index|
+			arr[item_index] = four_split(@rules[item])
 		end
-		puts count
-		 
-		# count = @rules[@rules["####".split("")]].count("#") +
-		# 		@rules[@rules[".###".split("")]].count("#") +
-		# 		@rules[@rules["####".split("")]].count("#") +
-		# 		@rules[@rules["#.#.".split("")]].count("#") +
-		# 		@rules[@rules["####".split("")]].count("#") +
-		# 		@rules[@rules[".###".split("")]].count("#") +
-		# 		@rules[@rules["####".split("")]].count("#") +
-		# 		@rules[@rules["#.#.".split("")]].count("#") +
-		# 		@rules[@rules["....".split("")]].count("#") +
-		# 		@rules[@rules["####".split("")]].count("#") +
-		# 		@rules[@rules[".#..".split("")]].count("#") +
-		# 		@rules[@rules[".#..".split("")]].count("#") +
-		# 		@rules[@rules["###.".split("")]].count("#") +
-		# 		@rules[@rules["#..#".split("")]].count("#") +
-		# 		@rules[@rules["...#".split("")]].count("#") +
-		# 		@rules[@rules["#...".split("")]].count("#")
-		#puts count
+		puts arr.flatten.count("#")
+
+		arr.each_with_index do |item, item_index|
+			item.each_with_index do |item2, item2_index|
+				arr[item_index][item2_index] = four_split(@rules[item2])
+			end
+		end
+		puts arr.flatten.count("#")
+
+		arr.each_with_index do |item, item_index|
+			item.each_with_index do |item2, item2_index|
+				item2.each_with_index do |item3, item3_index|
+					arr[item_index][item2_index][item3_index] = @rules[item3]
+				end
+			end
+		end
+		print arr
+		puts arr.flatten.count("#")
 		output
 	end
 
@@ -45,26 +44,12 @@ class Part1
 				line = line.chomp.delete("//").split(" => ")
 				find = line[0].split("")
 				if find.length == 4
-					perms = [find]
-					perms += two_by_rotations(find)
-					flips = two_by_flip(find)
-					perms += flips
-					flips.each do |flip|
-						perms += two_by_rotations(flip)
-					end
-					perms.uniq
+					perms = two_by_rotations(find)
 					perms.each do |perm|
 						@rules[perm] = line[1].split("")
 					end
 				else
-					perms = [find]
-					perms += three_by_rotations(find)
-					flips = three_by_flip(find)
-					perms += flips
-					flips.each do |flip|
-						perms += three_by_rotations(flip)
-					end
-					perms.uniq
+					perms = three_by_rotations(find)
 					perms.each do |perm|
 						@rules[perm] = line[1].split("")
 					end
@@ -82,53 +67,29 @@ class Part1
 		return split
 	end
 
-	def two_by_flip(arr)
-		flips = []
-
-		first = arr.clone
-		first.insert(0, first.delete_at(1))
-		first.insert(arr.length - 1, first.delete_at(2))
-		flips.push(first)
-
-		second = arr.clone
-		second.insert(0, second.delete_at(3))
-		second.insert(0, second.delete_at(3))
-		flips.push(second)
-		return flips
-	end
-
 	def two_by_rotations(arr)
-		rotations = []
-
-		first = arr.clone
-		first.insert(arr.length - 1, first.delete_at(1))
-		first.insert(0, first.delete_at(1))
-		rotations.push(first)
-
+		rotations = [arr]
+		rotations.push([arr[0], arr[2], arr[1], arr[3]])
+		rotations.push([arr[1], arr[0], arr[3], arr[2]])
+		rotations.push([arr[1], arr[3], arr[0], arr[2]])
+		rotations.push([arr[2], arr[0], arr[3], arr[1]])
+		rotations.push([arr[2], arr[3], arr[0], arr[1]])
+		rotations.push([arr[3], arr[1], arr[2], arr[0]])
 		rotations.push(arr.reverse)
-
-		third = arr.clone
-		third.insert(0, third.delete_at(3))
-		third.insert(0, third.delete_at(2))
-		rotations.push(third)
-		return rotations
-	end
-
-	def three_by_flip(arr)
-		flips = []
-
-		flips.push([arr[6], arr[7], arr[8], arr[3], arr[4], arr[5], arr[0], arr[1], arr[2]])
-		flips.push([arr[2], arr[1], arr[0], arr[5], arr[4], arr[3], arr[8], arr[7], arr[6]])
-		return flips
+		return rotations.uniq
 	end
 
 	def three_by_rotations(arr)
-		rotations = []
-
+		rotations = [arr]
 		rotations.push([arr[6], arr[3], arr[0], arr[7], arr[4], arr[1], arr[8], arr[5], arr[2]])
-		rotations.push(arr.reverse)
+		rotations.push([arr[8], arr[7], arr[6], arr[5], arr[4], arr[3], arr[2], arr[1], arr[0]])
 		rotations.push([arr[2], arr[5], arr[8], arr[1], arr[4], arr[7], arr[0], arr[3], arr[6]])
-		return rotations
+		rotations.push([arr[2], arr[1], arr[0], arr[5], arr[4], arr[3], arr[8], arr[7], arr[6]])
+		rotations.push([arr[8], arr[5], arr[2], arr[7], arr[4], arr[1], arr[6], arr[3], arr[0]])
+		rotations.push([arr[6], arr[7], arr[8], arr[3], arr[4], arr[5], arr[0], arr[1], arr[2]])
+		rotations.push([arr[0], arr[3], arr[6], arr[1], arr[4], arr[7], arr[2], arr[5], arr[8]])
+		rotations.push([arr[2], arr[1], arr[0], arr[5], arr[4], arr[3], arr[8], arr[7], arr[6]])
+		return rotations.uniq
 	end
 
 	def output
