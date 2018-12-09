a8f99e2df20a9e57cc5ba10856d7872baf63cc9d
susanev <susan.ra.evans@gmail.com>
2018-12-09 12:06:28 -0800

    camel case to snake case on method names

--- 2018/day09/part1.rb
+++ 2018/day09/part1.rb
@@ -24,13 +24,13 @@ end
 class Part1
 	def initialize(file_name)
 		@curr_player = 2
-		processFile(file_name)
+		process_file(file_name)
 		initalize_list
-		playGame
+		play_game
 		output
 	end
 
-	def processFile(file_name)
+	def process_file(file_name)
 		@players, @total_marbles = File.open(file_name, "r").
 				readlines[0].scan(/\d+/).map(&:to_i)
 		@scores = Array.new(@players, 0)
@@ -45,7 +45,7 @@ class Part1
 		@list = List.new(start_node, start_node)
 	end
 
-	def playGame
+	def play_game
 		while @curr_marble < @total_marbles
 			if @curr_marble % 23 == 0
 				@list.curr_node = @list.curr_node.prev_node.prev_node.prev_node.
@@ -53,10 +53,10 @@ class Part1
 
 				@scores[@curr_player] += @curr_marble +
 							@list.curr_node.value
-				removeNode
+				remove_node
 			else
 				@list.curr_node = @list.curr_node.next_node
-				addNode
+				add_node
 			end
 
 			@curr_marble += 1
@@ -64,14 +64,14 @@ class Part1
 		end
 	end
 
-	def removeNode
+	def remove_node
 		save = @list.curr_node.next_node			
 		@list.curr_node.prev_node.next_node = @list.curr_node.next_node
 		@list.curr_node.next_node.prev_node = @list.curr_node.prev_node
 		@list.curr_node = save
 	end
 
-	def addNode
+	def add_node
 		new_node = Node.new(@curr_marble, @list.curr_node.next_node,
 				@list.curr_node)
 		@list.curr_node.next_node = new_node
