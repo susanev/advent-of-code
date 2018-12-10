cded101e73e7ec9a5c32fcfdef7f5f4f7287f602
susanev <susan.ra.evans@gmail.com>
2018-12-09 19:59:09 -0800

    removed unneccesary curr marble instance var

--- 2018/day09/part1.rb
+++ 2018/day09/part1.rb
@@ -23,7 +23,6 @@ end
 
 class Part1
 	def initialize(file_name)
-		@curr_player = 2
 		process_file(file_name)
 		initalize_list
 		play_game
@@ -34,11 +33,10 @@ class Part1
 		@players, @total_marbles = File.open(file_name, "r").
 				readlines[0].scan(/\d+/).map(&:to_i)
 		@scores = Array.new(@players, 0)
+		@curr_player = 2
 	end
 
 	def initalize_list
-		@curr_marble = 1
-
 		start_node = Node.new(0, nil, nil)
 		start_node.next_node = start_node
 		start_node.prev_node = start_node
@@ -46,20 +44,19 @@ class Part1
 	end
 
 	def play_game
-		while @curr_marble < @total_marbles
-			if @curr_marble % 23 == 0
+		@total_marbles.times do |curr_marble|
+			if curr_marble % 23 == 0
 				@list.curr_node = @list.curr_node.prev_node.prev_node.prev_node.
 						prev_node.prev_node.prev_node.prev_node
 
-				@scores[@curr_player] += @curr_marble +
+				@scores[@curr_player] += curr_marble +
 							@list.curr_node.value
 				remove_node
 			else
 				@list.curr_node = @list.curr_node.next_node
-				add_node
+				add_node(curr_marble)
 			end
 
-			@curr_marble += 1
 			@curr_player= (@curr_player + 1) % @players
 		end
 	end
@@ -70,8 +67,8 @@ class Part1
 		@list.curr_node = @list.curr_node.prev_node.next_node
 	end
 
-	def add_node
-		new_node = Node.new(@curr_marble, @list.curr_node.next_node,
+	def add_node(curr_marble)
+		new_node = Node.new(curr_marble, @list.curr_node.next_node,
 				@list.curr_node)
 		@list.curr_node.next_node = new_node
 		@list.curr_node = new_node
--- 2018/day09/part2.rb
+++ 2018/day09/part2.rb
@@ -23,7 +23,6 @@ end
 
 class Part2
 	def initialize(file_name)
-		@curr_player = 2
 		process_file(file_name)
 		initalize_list
 		play_game
@@ -35,11 +34,10 @@ class Part2
 				readlines[0].scan(/\d+/).map(&:to_i)
 		@total_marbles *= 100
 		@scores = Array.new(@players, 0)
+		@curr_player = 2
 	end
 
 	def initalize_list
-		@curr_marble = 1
-
 		start_node = Node.new(0, nil, nil)
 		start_node.next_node = start_node
 		start_node.prev_node = start_node
@@ -47,32 +45,31 @@ class Part2
 	end
 
 	def play_game
-		while @curr_marble < @total_marbles
-			if @curr_marble % 23 == 0
+		@total_marbles.times do |curr_marble|
+			if curr_marble % 23 == 0
 				@list.curr_node = @list.curr_node.prev_node.prev_node.prev_node.
 						prev_node.prev_node.prev_node.prev_node
 
-				@scores[@curr_player] += @curr_marble +
+				@scores[@curr_player] += curr_marble +
 							@list.curr_node.value
 				remove_node
 			else
 				@list.curr_node = @list.curr_node.next_node
-				add_node
+				add_node(curr_marble)
 			end
 
-			@curr_marble += 1
 			@curr_player= (@curr_player + 1) % @players
 		end
 	end
 
-	def remove_node		
+	def remove_node
 		@list.curr_node.prev_node.next_node = @list.curr_node.next_node
 		@list.curr_node.next_node.prev_node = @list.curr_node.prev_node
 		@list.curr_node = @list.curr_node.prev_node.next_node
 	end
 
-	def add_node
-		new_node = Node.new(@curr_marble, @list.curr_node.next_node,
+	def add_node(curr_marble)
+		new_node = Node.new(curr_marble, @list.curr_node.next_node,
 				@list.curr_node)
 		@list.curr_node.next_node = new_node
 		@list.curr_node = new_node
