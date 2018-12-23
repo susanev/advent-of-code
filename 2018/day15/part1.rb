694d1806d73ff69bd43ef37d7a2668adddd2a665
susanev <susan.ra.evans@gmail.com>
2018-12-22 16:29:51 -0800

    progress but no success

--- 2018/day15/part1.rb
+++ 2018/day15/part1.rb
@@ -5,93 +5,64 @@
 # generating a map of the walls (#),
 # open cavern (.), and starting position
 # of every Goblin (G) and Elf (E) (your puzzle input).
+
 class PriorityQueue
 	def initialize
-		@elements = []
-		@size = 0
+		@list = []
 	end
 
 	def push(element)
-		@elements.push(element)
-		@size += 1
+		@list.push(element)
 	end
 
 	def pop
-		@elements.sort!
-		item = @elements.delete_at(@size - 1)
-		@size -= 1
-		return item
+		@list = @list.sort.reverse
+		return @list.pop
 	end
 
 	def empty?
-		return @elements.empty?
+		return @list.length == 0
+	end
+
+	def to_s
+		output = ""
+		@list.each do |item|
+			output << item.to_s + ", "
+		end
+		return output
 	end
 end
 
 class Element
-	attr_accessor :monster, :priority
+	TYPE_TO_STR = {"." => :open, "#" => :wall, "E" => :E, "G" => :G}
 
-	def initialize(monster, priority)
-		@monster = monster
-		@priority = priority
-	end
+	attr_accessor :x, :y, :type, :priority, :attack, :hit, :str
 
-	def <=>(other)
-		@priority <=> other.priority
-	end
-
-	def to_s
-		return monster.to_s
-	end
-end
-
-class Monster
-	attr_accessor :x, :y, :type, :attack, :hit
-
-	def initialize(x, y, type)
+	def initialize(x, y, str)
 		@x = x
 		@y = y
-		@type = type
-		@attack = 3
-		@hit = 200
-	end
-
-	def to_s
-		return "x: #{@x} y: #{@y} type: #{@type.to_s}"
+		@type = TYPE_TO_STR[str]
+		@str = str
+		@priority = 0
+		if type == :E || type == :G
+			@attack = 3
+			@hit = 200
+		end
 	end
 
 	def <=>(other)
 		return [@y, @x] <=> [other.y , other.x]
 	end
-end
 
-class Wall
-	attr_accessor :x, :y, :type
-
-	def initialize(x, y, type)
-		@x = x
-		@y = y
-		@type = type
-	end
 	def to_s
-		return "x: #{@x} y: #{@y} type: #{@type.to_s}"
-	end
-end
-
-class Open
-	attr_accessor :x, :y, :type
-
-	def initialize(x, y, type)
-		@x = x
-		@y = y
-		@type = type
-	end
-	def to_s
-		return "x: #{@x} y: #{@y} type: #{@type.to_s}"
+		return @str
+		#return "x: " + @x.to_s + " y: " + @y.to_s + " " + @str
 	end
 end
 
 class Part1
+	SQUARES = [[0, -1], [-1, 0], [1, 0], [0, 1]]
+
 	def initialize(file_name)
 		@width = nil
 		@height = nil
@@ -99,8 +70,12 @@ class Part1
 		@elves = []
 		@goblins = []
 		process_file(file_name)
-		next_move
-		#print_map
+		# find_monsters
+		4.times do
+		turn
+	end
+		#game
+		print_map
 		#output
 	end
 
@@ -120,115 +95,211 @@ class Part1
 		@height = input.length
 		@width = input[0].length
 		for y in 0...@height
-			for x in 0...@width
-				type = input[y][x]
-				if type == "E" || type == "G"
-					monster = Monster.new(x, y, type.to_sym)
-					@map[[x,y]] = monster
-					if type == "E"
-						@elves.push(monster)
-					else # G
-						@goblins.push(monster)
-					end
-				elsif type == "."
-					@map[[x,y]] = Open.new(x, y, type)
-				else #wall
-					@map[[x,y]] = Wall.new(x, y, type)
+			for x in 0...@width			
+				@map[[x,y]] = Element.new(x, y, input[y][x])
+			end
+		end
+	end
+
+	def find_monsters
+		@elves = []
+		@goblins = []
+		for y in 0...@height
+			for x in 0...@width			
+				if @map[[x,y]].type == :E
+					@elves.push(@map[[x,y]])
+				elsif @map[[x,y]].type == :G
+					@goblins.push(@map[[x,y]])
 				end
 			end
 		end
 	end
 
-	def next_move
-		monsters = (@elves + @goblins).sort
+	def turn
+		find_monsters
+		@monsters = (@elves + @goblins).sort
 
-		find_shortest_path(monsters[0], @elves[0])
-		# monsters.each do |monster|
-		# 	if monster.type == :E
-		# 		@goblins.each do |goblin|
-		# 			find_shortest_path(monster, goblin)
-		# 		end
-		# 	else
-		# 		@elves.each do |elf|
-		# 			find_shortest_path(monster, elf)
-		# 		end
-		# 	end
-		# end
+		@all_moves = []
+		@monsters.each do |monster|
+			target = find_targets(monster)
+			if target != nil
+				target.hit -= monster.attack
+				if target.hit <= 0
+					target.type = :open
+					target.str = "."
+				end
+			elsif find_open_squares(monster).length > 0
+				@all_moves.push(move(monster))
+			end
+		end
+
+		@all_moves.each do |move|
+			prev_x = move[:monster].x
+			prev_y = move[:monster].y
+			new_x = move[:path].x
+			new_y = move[:path].y
+			type = move[:monster].type
+			str = move[:monster].str
+			attack = move[:monster].attack
+			hit = move[:monster].hit
+			@map[[prev_x, prev_y]].type = :open
+			@map[[prev_x, prev_y]].str = "."
+			@map[[new_x, new_y]].type = type
+			@map[[new_x, new_y]].str = str	
+			@map[[new_x, new_y]].hit = hit
+			@map[[new_x, new_y]].attack = attack		
+		end
 	end
 
-	def find_shortest_path(start, goal)
-		frontier = PriorityQueue.new
-		ele = Element.new(start, 0)
-		frontier.push(ele)
-		came_from = {}
-		cost_so_far = {}
-		came_from[ele] = nil
-		cost_so_far[ele] = 0
+	def find_targets(monster)
+		look_for = nil
+		if monster.type == :E
+			look_for = :G
+		else # :G
+			look_for = :E
+		end
+		targets = []
+		SQUARES.each do |square|
+			x = monster.x + square[0]
+			y = monster.y + square[1]
+			if @map[[x, y]].type == look_for
+				targets.push(@map[[x, y]])
+			end
+		end
+		if targets.length == 0
+			return nil
+		else
+			return find_smallest_hit_points(targets)
+		end
+	end
 
-		while !frontier.empty?
-   			current = frontier.pop()
-   			if off_by_one(current, goal)
-      			break
-      		end
-   
-   			get_neighbors(current).each do |neighbor|
-   				puts cost_so_far[current]
-      			new_cost = cost_so_far[current] + current.priority + 1
-			    if !cost_so_far[neighbor] || new_cost < cost_so_far[neighbor]
-			    	cost_so_far[neighbor] = new_cost
-			        priority = new_cost + heuristic(goal, neighbor)
-			        frontier.push(Element.new(neighbor, priority))
-			        came_from[neighbor] = current
-			    end
+	def find_smallest_hit_points(targets)
+		smallest = targets.map{ |target| target.hit }.min
+		targets.each do |target|
+			if target.hit == smallest
+				return target
 			end
 		end
 	end
 
-	def heuristic(a, b)
-		return (a.x - b.x).abs + (a.y - b.y).abs
+	def find_open_squares(monster)
+		open_squares = []
+		SQUARES.each do |square|
+			x = monster.x + square[0]
+			y = monster.y + square[1]
+			if @map[[x, y]].type == :open
+				open_squares.push(@map[[x,y]])
+			end
+		end
+		return open_squares
 	end
 
-	def off_by_one(current, goal)
-		return (current.monster.x - 1 == goal.x &&
-					current.monster.y == goal.y) ||
-				(current.monster.x + 1 == goal.x &&
-					current.monster.y == goal.y) ||
-				(current.monster.x  == goal.x &&
-					current.monster.y + 1 == goal.y) ||
-				(current.monster.x == goal.x &&
-					current.monster.y - 1 == goal.y) 
- 	end
+	def move(monster)
+		moves = []
+		if monster.type == :E
+			@goblins.each do |goblin|
+				open_squares = find_open_squares(goblin)
+				distances = []
+				open_squares.each do |square|
+					distances.push(get_distance(monster, square))
+				end
+				shortest = find_shortest_distance(distances)
+				moves.push({monster: monster, path: shortest[0], cost: shortest[1]})
+			end
+		else # elf
+			@elves.each do |elf|
+				open_squares = find_open_squares(elf)
+				distances = []
+
+				open_squares.each do |square|
+					distances.push(get_distance(monster, square))
+				end
+				shortest = find_shortest_distance(distances)
+				moves.push({monster: monster, path: shortest[0], cost: shortest[1]})
+			end
+		end
+
+		chosen_path = moves.map{ |move| move[:cost] }.min
+		moves.each do |move|
+			if move[:cost] == chosen_path
+				return move
+			end
+		end
+	end
+
+	def find_shortest_distance(distances)
+		shortest = distances.map{ |distance| distance[1] }.min
+		distances.each do |distance|
+			if distance[1] == shortest
+				return distance
+			end
+		end
+	end		
+
+ 	def get_distance(start, goal)
+		frontier = PriorityQueue.new
+		frontier.push(start)
+		came_from = {}
+		came_from[start] = nil
+		cost_so_far = {}
+		cost_so_far[start] = 0
+
+		while !frontier.empty?
+			current = frontier.pop
+
+			if current == goal
+				break
+			end
+
+			neighbors = get_neighbors(current).sort
+
+			neighbors.each do |neighbor|
+				new_cost = cost_so_far[current] + 1
+				if (cost_so_far[neighbor] == nil || new_cost < cost_so_far[neighbor])
+					cost_so_far[neighbor] = new_cost
+					neighbor.priority = new_cost
+					frontier.push(neighbor)
+					came_from[neighbor] = current
+				end
+			end
+		end
+
+		current = goal
+		path = []
+
+		while current != start
+			path.push(current)
+			current = came_from[current]
+		end
+
+		choice = path.pop
+
+		return [choice, cost_so_far[goal]]
+	end
+
+	# def heuristic(a, b)
+	# 	return (a.x - b.x).abs + (a.y - b.y).abs
+	# end
 
 	def get_neighbors(current)
 		neighbors = []
-		x = current.monster.x
-		y = current.monster.y
-		if valid_move(x + 1, y)
-			neighbors.push(@map[[x+1, y]])
+		SQUARES.each do |square|
+			x = current.x + square[0]
+			y = current.y + square[1]
+			if valid_move(x, y)
+				neighbors.push(@map[[x,y]])
+			end
 		end
-		if valid_move(x - 1, y)
-			neighbors.push(@map[[x-1, y]])
-		end
-		if valid_move(x, y + 1)
-			neighbors.push(@map[[x, y+1]])
-		end
-		if valid_move(x + 1, y - 1)
-			neighbors.push(@map[[x, y-1]])
-		end
-		return neighbors
+		return neighbors.sort
 	end
 
 	def valid_move(x, y)
 		return x > -1 && x < @width && 
 				y > -1 && y < @height &&
-				@map[[x,y]].type == "."
-	end
-
-
-	def output
-		puts "#{}"
+				@map[[x,y]].type == :open
 	end
 end
 
 Part1.new("input.txt")
+
 
